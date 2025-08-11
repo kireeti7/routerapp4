9b949e5e2eb759f04965c625d146d80ffc685add
kireeti7 <dannasikireeti@gmail.com>
2025-08-11 12:10:57 +0530

    added styles to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
-  return (
-    <nav>
-      <Link to='/' >Home</Link>
-      <Link to='/about' >Home</Link>
-    </nav>
-  )
+    const navLinkStyles = ({ isActive }) => {
+        return {
+            fontWeight: isActive ? 'bold' : 'normal',
+            textDecoration: isActive ? 'none' : 'underline',
+        }
+    }
+    return (
+        <nav>
+            <NavLink style={navLinkStyles} to='/' >Home</NavLink>
+            <NavLink style={navLinkStyles} to='/about' >About</NavLink>
+        </nav>
+    )
 }
 
 export default NavBar
